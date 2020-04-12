c1a0030d8f88f98afd1b4962d87152b772c3a8a8
renatopcinfo <renatopcinfo@gmail.com>
2020-04-11 22:53:05 -0300

    contacts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
+import Contacts from './containers/Contacts';
 
 function App() {
   return (
     <>
-      <div className="App"></div>
+      <Contacts />
       <Styles />
     </>
   );
@@ -13,6 +14,7 @@ function App() {
 const Styles = createGlobalStyle`
   html {
     font-size: 16px;
+    font-family: sans-serif;
   }
 
   html, body, #root {
